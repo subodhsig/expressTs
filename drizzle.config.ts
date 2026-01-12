ff05bee5d88a403edf6b53ce45f516cd6c67124e
subodh <subodhsigdel63@gmail.com>
2026-01-12 17:01:47 +0545

    minimal chnages in dbconfig of db url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "drizzle-kit";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { config } from "./src/config/config";
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schemas/*.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: config.dbConfig.url!,
   },
   verbose: true,
   strict: true,
