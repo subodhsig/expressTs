347626dfc60d7bd681978d72bbc6db30764c3dd8
subodh <subodhsigdel63@gmail.com>
2026-01-13 10:13:11 +0545

    user deleted implement

--- src/services/users/user.service.ts
+++ src/services/users/user.service.ts
@@ -16,6 +16,14 @@ class UserServices {
     return users[0] || null;
   }
 
+  async getUserById(id: number): Promise<User | null> {
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, id));
+    return user[0] || null;
+  }
+
   async createUser(userData: NewUser): Promise<User> {
     const existingUser = await this.getUserByEmail(userData.email);
     if (existingUser) {
@@ -24,6 +32,31 @@ class UserServices {
     const result = await db.insert(usersTable).values(userData).returning();
 
     return result[0];
+  }
+
+  async updateUser(userData: User, id: number): Promise<User> {
+    const existingUser = await this.getUserById(id);
+    if (!existingUser) {
+      throw new Error("user not found");
+    }
+    const [updatedUser] = await db
+      .update(usersTable)
+      .set({ name: userData.name, age: userData.age, email: userData.email })
+      .where(eq(usersTable.id, userData.id))
+      .returning();
+    return updatedUser;
+  }
+
+  async deleteUser(id: number) {
+    const existingUser = await this.getUserById(id);
+    if (!existingUser) {
+      throw new Error("User not Found");
+    }
+    const [deletedUser] = await db
+      .delete(usersTable)
+      .where(eq(usersTable.id, id))
+      .returning();
+    return deletedUser;
   }
 }
 
