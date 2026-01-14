7db5d5ec12b09dc242648dd23d0b8beb249d6fe0
subodh <subodhsigdel63@gmail.com>
2026-01-14 10:46:15 +0545

    added references in schema

--- src/db/schemas/product.schema.ts
+++ src/db/schemas/product.schema.ts
@@ -1,10 +1,13 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { usersTable } from "./user.schema";
 
 export const productTable = pgTable("products", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar().notNull(),
   description: varchar().notNull(),
-  userId: integer().notNull(),
+  userId: integer()
+    .notNull()
+    .references(() => usersTable.id),
 });
 export type Prodcut = typeof productTable.$inferSelect;
 export type NewProduct = typeof productTable.$inferInsert;
